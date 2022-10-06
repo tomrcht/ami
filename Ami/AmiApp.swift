ffc661604f97edaeaf50b1f19e390fed51d082a6
tomrcht <tom.roch4t@gmail.com>
2022-10-06 22:05:11 +0200

    Initial Commit

---
+++ Ami/AmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmiApp.swift
+//  Ami
+//
+//  Created by Tom Rochat on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
