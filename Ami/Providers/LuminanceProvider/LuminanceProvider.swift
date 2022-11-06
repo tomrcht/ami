93aa4029a68f7f1376e3ebadd4d5da015d80a646
tomrcht <tom.roch4t@gmail.com>
2022-11-06 10:49:19 +0100

    Add complex luminance calculation

--- Ami/Providers/LuminanceProvider/LuminanceProvider.swift
+++ Ami/Providers/LuminanceProvider/LuminanceProvider.swift
@@ -9,6 +9,8 @@ import Foundation
 
 protocol LuminanceProviding {
     func simple(red: Double, green: Double, blue: Double) -> Double
+    func complex(red: Double, green: Double, blue: Double) -> Double
+    func perceivedLightness(luminance: Double) -> Double
 }
 
 struct LuminanceProvider: LuminanceProviding {
@@ -21,23 +23,67 @@ struct LuminanceProvider: LuminanceProviding {
     /// Source : [Relative luminance Wikipedia page](https://en.wikipedia.org/wiki/Relative_luminance)
     ///
     /// - parameters:
+    ///     - red: The red component of the color, must be in range 0...1
+    ///     - green: The green component of the color, must be in range 0...1
+    ///     - blue: The blue component of the color, must be in range 0...1
+    /// - returns: A computed value for the luminance based on the given sRGB colors
+    func simple(red: Double, green: Double, blue: Double) -> Double {
+        assert((0...1).contains(red), "Red component is out of acceptable range")
+        assert((0...1).contains(green), "Green component is out of acceptable range")
+        assert((0...1).contains(blue), "Blue component is out of acceptable range")
+
+        return 0.2126 * red + 0.7152 * green + 0.0722 * blue
+    }
+
+    /// Calculates the luminance for a given sRGB color. Unlinke the `simple` method, this solution takes into account
+    /// the fact that sRGB values *needs* to be linearized
+    ///
+    /// All values are pulled from the [sRGB Wikipedia page](https://en.wikipedia.org/wiki/SRGB)
+    ///
+    /// - parameters:
     ///     - red: The red component of the color, must be in range 0...255
     ///     - green: The green component of the color, must be in range 0...255
     ///     - blue: The blue component of the color, must be in range 0...255
     /// - returns: A computed value for the luminance based on the given sRGB colors
-    func simple(red: Double, green: Double, blue: Double) -> Double {
-        assert((0...255).contains(red), "Red component is out of acceptable range")
-        assert((0...255).contains(green), "Green component is out of acceptable range")
-        assert((0...255).contains(blue), "Blue component is out of acceptable range")
+    func complex(red: Double, green: Double, blue: Double) -> Double {
+        assert((0...1).contains(red), "Red component is out of acceptable range")
+        assert((0...1).contains(green), "Green component is out of acceptable range")
+        assert((0...1).contains(blue), "Blue component is out of acceptable range")
 
-        return 0.2126 * red + 0.7152 * green + 0.0722 * blue
+        let linearRed = linearizeToCIEXYZ(sRGB: red)
+        let linearGreen = linearizeToCIEXYZ(sRGB: green)
+        let linearBlue = linearizeToCIEXYZ(sRGB: blue)
+
+        return 0.2126 * linearRed + 0.7152 * linearGreen + 0.0722 * linearBlue
+    }
+
+    /// Caculates the perceived lightness based on the luminance of a given color
+    func perceivedLightness(luminance: Double) -> Double {
+        assert((0...1).contains(luminance), "Luminance is out of acceptable range")
+
+        if luminance <= 0.008856 {
+            return luminance * 903.3
+        } else {
+            return pow(luminance, 1 / 3) * 116 - 16
+        }
+    }
+
+    /// Linearizes an sRGB value to the CIE XYZ color space
+    private func linearizeToCIEXYZ(sRGB value: Double) -> Double {
+        assert((0...1).contains(value), "Value is out of acceptable range")
+
+        if value < 0.04045 {
+            return value / 12.92
+        } else {
+            return pow((value + 0.055) / 1.055, 2.4)
+        }
     }
 }
 
 #if DEBUG
 struct LuminanceProviderMock: LuminanceProviding {
-    func simple(red: Double, green: Double, blue: Double) -> Double {
-        0.0
-    }
+    func simple(red: Double, green: Double, blue: Double) -> Double { 0.0 }
+    func complex(red: Double, green: Double, blue: Double) -> Double { 0.0 }
+    func perceivedLightness(luminance: Double) -> Double { 0.0 }
 }
 #endif
--- Ami/Views/Luminance/LuminanceView.swift
+++ Ami/Views/Luminance/LuminanceView.swift
@@ -7,70 +7,161 @@
 
 import SwiftUI
 
+private enum LuminanceAlgorithm: Identifiable, Equatable {
+    case simple
+    case complex
+
+    var id: Self { self }
+}
+
+private enum TextColor: Identifiable, Equatable {
+    case auto
+    case light
+    case dark
+
+    var id: Self { self }
+}
+
 struct LuminanceView: View {
     @State private var rgbValues = RGBValues(red: 0, green: 0, blue: 0)
     @State private var luminance = 0.0
+    @State private var perceivedLightness = 0.0
+
+    @State private var algorithm = LuminanceAlgorithm.simple
+    @State private var textColor = TextColor.auto
     @State private var showText = true
 
     @Injected(\.luminanceProvider) private var luminanceProvider
     @EnvironmentObject private var theme: ThemeManager
 
-    private let exampleText = """
+    private let threshold = 0.179
+    private let sampleText = """
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
     """
 
     var body: some View {
-        VStack(spacing: 8) {
-            LuminanceColorSlider(value: $rgbValues.red, tintColor: .red)
-            LuminanceColorSlider(value: $rgbValues.green, tintColor: .green)
-            LuminanceColorSlider(value: $rgbValues.blue, tintColor: .blue)
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 8) {
+                // MARK: - Colors + render
+                LuminanceColorSlider(value: $rgbValues.red, tintColor: .red)
+                LuminanceColorSlider(value: $rgbValues.green, tintColor: .green)
+                LuminanceColorSlider(value: $rgbValues.blue, tintColor: .blue)
 
-            ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(rgbValues.color)
-                    .frame(height: 255)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(rgbValues.color)
+                        .frame(height: 255)
 
-                if showText {
-                    Text(exampleText)
-                        .foregroundColor(.white)
-                        .font(.regular(ofSize: 20))
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 24)
+                    if showText {
+                        Text(sampleText)
+                            .foregroundColor(getSampleTextColor())
+                            .font(.regular(ofSize: 20))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 24)
+                    }
                 }
-            }
-            .padding(.vertical, 24)
+                .padding(.vertical, 24)
 
-            HStack {
-                Text("Luminance value is \(luminance)")
-                    .foregroundColor(theme.current.text)
-                    .font(.heavy(ofSize: 16))
+                // MARK: - informations
+                infoView
 
-                Spacer()
-            }
+                Divider().padding(.vertical, 8)
 
-            Divider()
-                .padding(.vertical, 8)
-
-            HStack {
-                Text("Settings")
-                    .foregroundColor(theme.current.text)
-                    .font(.heavy(ofSize: 16))
+                // MARK: - Settings
+                settingsView
 
                 Spacer()
             }
-
-            Toggle(isOn: $showText) {
-                Text("Show text")
-                    .foregroundColor(theme.current.text)
-            }.tint(theme.current.primary)
-
-            Spacer()
         }
         .padding(.horizontal, 24)
         .background(theme.current.background)
         .onChange(of: rgbValues) { _ in
-            luminance = luminanceProvider.simple(red: rgbValues.red, green: rgbValues.green, blue: rgbValues.blue)
+            let red = rgbValues.red / 255
+            let green = rgbValues.green / 255
+            let blue = rgbValues.blue / 255
+
+            if algorithm == .simple {
+                luminance = luminanceProvider.simple(red: red, green: green, blue: blue)
+            } else {
+                luminance = luminanceProvider.complex(red: red, green: green, blue: blue)
+                perceivedLightness = luminanceProvider.perceivedLightness(luminance: luminance)
+            }
+        }
+    }
+
+    private var infoView: some View {
+        VStack {
+            HStack {
+                Text("Luminance value is \(luminance)")
+                    .foregroundColor(theme.current.text)
+                    .font(.heavy(ofSize: 16))
+                Spacer()
+            }
+
+            if algorithm == .complex {
+                HStack {
+                    Text("Perceived lightness value is \(perceivedLightness)")
+                        .foregroundColor(theme.current.text)
+                        .font(.heavy(ofSize: 16))
+                    Spacer()
+                }
+            }
+        }
+    }
+
+    private var settingsView: some View {
+        VStack {
+            HStack {
+                Text("Settings")
+                    .foregroundColor(theme.current.text)
+                    .font(.heavy(ofSize: 16))
+                Spacer()
+            }
+
+            Toggle(isOn: $showText) {
+                Text("Show sample text")
+                    .foregroundColor(theme.current.text)
+                    .font(.regular(ofSize: 16))
+            }.tint(theme.current.primary)
+
+            HStack {
+                Text("Text color")
+                    .foregroundColor(theme.current.text)
+                    .font(.regular(ofSize: 16))
+                Spacer()
+                Picker("Text color", selection: $textColor) {
+                    Text("Auto").tag(TextColor.auto)
+                    Text("White").tag(TextColor.light)
+                    Text("Black").tag(TextColor.dark)
+                }
+                .pickerStyle(.menu)
+                .tint(theme.current.primary)
+            }
+
+            HStack {
+                Text("Algorithm to use")
+                    .foregroundColor(theme.current.text)
+                    .font(.regular(ofSize: 16))
+                Spacer()
+                Picker("Algorithm", selection: $algorithm) {
+                    Text("Simple").tag(LuminanceAlgorithm.simple)
+                    Text("Complex").tag(LuminanceAlgorithm.complex)
+                }
+                .pickerStyle(.menu)
+                .tint(theme.current.primary)
+            }
+        }
+    }
+
+    private func getSampleTextColor() -> Color {
+        switch textColor {
+        case .auto:
+            return .red
+        case .light:
+            return .white
+        case .dark:
+            return .black
         }
     }
 }
